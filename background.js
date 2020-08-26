2ba404e821989ebf3f5f06906fc92577be8856dc
Hardelele <Hardelele@yahoo.com>
2020-08-27 01:34:12 +0300

    Update version

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'github.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'github.com'}
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
 });
